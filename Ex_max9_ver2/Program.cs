7161eeaad06352fc88376419d5f443210710a767
pidzhak <pidzhak84@gmail.com>
2023-01-18 00:03:30 +0300

    Вариант с функциональным программированием

--- Ex_max9_ver2/Program.cs
+++ Ex_max9_ver2/Program.cs
@@ -16,11 +16,13 @@ int b2 = 23;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
-int c3 = 3367;
+int c3 = 333;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 System.Console.WriteLine(max);
